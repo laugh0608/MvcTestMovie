9d134f009c2d7fe003c5b760222f568d4ce2ab30
laugh0608 <laugh608@163.com>
2025-04-04 19:17:50 +0800

    修改Welcome方法的访问返回值

--- MvcTestMovie/Controllers/HelloWorldController.cs
+++ MvcTestMovie/Controllers/HelloWorldController.cs
@@ -11,10 +11,20 @@ public class HelloWorldController : Controller
     {
         return "This is my default action...";
     }
-    // 
+
     // GET: /HelloWorld/Welcome/ 
-    public string Welcome()
+    // Requires using System.Text.Encodings.Web;
+    // 使用 C# 可选参数功能指示，未为 numTimes 参数传递值时该参数默认为 1
+    // public string Welcome(string name, int numTimes = 1)
+    // {
+    //     // return "This is the Welcome action method...";
+    //     
+    //     // 使用 HtmlEncoder.Default.Encode 防止恶意输入（例如通过 JavaScript）损害应用。
+    //     return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    //     // 访问 URL：https://localhost:{PORT}/HelloWorld/Welcome?name=Rick&numtimes=4，输出：Hello Rick, NumTimes is: 4
+    // }
+    public string Welcome(string name, int ID = 1)
     {
-        return "This is the Welcome action method...";
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
     }
 }
